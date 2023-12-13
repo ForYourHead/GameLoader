5ce197c9eba70c056491afb7845f388722b7ad77
r4mpage <144806538+ForYourHead@users.noreply.github.com>
2023-12-13 20:55:58 +0800

    Update r4mpageGameLoader.lua

--- r4mpageGameLoader.lua
+++ r4mpageGameLoader.lua
@@ -1,2 +1,15 @@
-if game.PlaceId == 13772394625 then
-    loadstring(game:HttpGet("https://paste.gg/p/R4mpage_/82c3f9a25fcc49d2a06ff084b71beb01/files/2591776934344982ab60d9303d257264/raw"))()
+repeat wait() until game:IsLoaded();
+if game.PlaceId == 13772394625 or game.PlaceId == 15185247558 or game.PlaceId == 14915220621 or game.PlaceId == 14732610803 then
+loadstring(game:HttpGet("https://paste.gg/p/R4mpage_/82c3f9a25fcc49d2a06ff084b71beb01/files/2591776934344982ab60d9303d257264/raw"))()
+else
+    game.Players.LocalPlayer:Kick("Map not found !")
+
+
+
+
+wait(2)
+
+
+  
+    game:Shutdown()
+end
