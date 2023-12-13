0984d173dac6bb42e0ef9d2e2b41071f60399cfe
r4mpage <144806538+ForYourHead@users.noreply.github.com>
2023-12-13 19:54:26 +0800

    Update r4mpageGameLoader.lua

--- r4mpageGameLoader.lua
+++ r4mpageGameLoader.lua
@@ -1,1 +1,4 @@
-
+if game.PlaceId ==  then
+    loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5621/20513/HarPy.lua'))()
+elseif game.PlaceId == 11445923563 or game.PlaceId == 13155198714 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/xShadow1234/HarPy/main/One%20Fruit.lua'))()
