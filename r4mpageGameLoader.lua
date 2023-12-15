a705c64735102cce3afee0f7517330ceecd44424
r4mpage <144806538+ForYourHead@users.noreply.github.com>
2023-12-15 14:55:10 +0800

    Update r4mpageGameLoader.lua

--- r4mpageGameLoader.lua
+++ r4mpageGameLoader.lua
@@ -1,8 +1,1 @@
-repeat wait() until game:IsLoaded()
-if game.PlaceId == 13772394625 then
 loadstring(game:HttpGet("https://paste.gg/p/R4mpage_/3d1767a1a2fe43838f733a0c0d5a1c32/files/61969cab5f11424f9f199ad779138866/raw"))()
-else
-    game.Players.LocalPlayer:Kick("Game Not Supported!")
-wait(0.1)
-    game:Shutdown(0.1)
-end
